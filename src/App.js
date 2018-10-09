e52d2e3f46ca6743ce4b75558dc5828b52f5e2c5
emily-yang <cigoua@gmail.com>
2018-10-09 14:11:50 -0700

    set up canvas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tesseract from 'tesseract.js';
 
 class App extends Component {
+
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    window.addEventListener('paste', handlePasteEvent);
+
+    function handlePasteEvent(event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      // do nothing if clipboard is empty
+      if (!event.clipboardData) {
+        console.log('nothing in clipboard!');
+        return;
+      }
+
+      const items = event.clipboardData.items;
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].type.includes('image')) {
+          const blob = items[i].getAsFile();
+          const ctx = canvas.getContext('2d');
+          const img = new Image();
+          img.onload = function() {
+            canvas.width = this.width;
+            canvas.height = this.height;
+            ctx.drawImage(img, 0, 0);
+          }
+          img.src = window.URL.createObjectURL(blob);
+
+          Tesseract.recognize(canvas)
+            .then(function(result){
+              console.log(result);
+            });
+        }
+      }
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <canvas ref="canvas" className="canvas"></canvas>
         </header>
       </div>
     );
