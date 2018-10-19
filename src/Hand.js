4b5489fd384cf3e453eb652ec6acd6ba34322e6d
emily-yang <cigoua@gmail.com>
2018-10-19 16:08:09 -0700

    make cards sortable by dragging

--- src/Hand.js
+++ src/Hand.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
+import DragSortableList from 'react-drag-sortable';
 
 class Hand extends Component {
+
+	onSort = (sortedList, dropEvent) => {
+		console.log('sortedList: ', sortedList, dropEvent);
+	}
 	render() {
 		let i = 1;
+		const cards = this.props.hand.map(img => {
+			return {content: (<img src={img} key={`card${i++}`} alt="" />), classes:["card"]}
+		});
+		console.log(cards);
 		return (
 			<div className="hand">
-				{(this.props.hand).map(img =>
-					<img className="card" draggable src={img} key={`card${i++}`} alt="" />
-				)}
+				<DragSortableList items={cards} type="horizontal" />
 			</div>
 		);
 	}
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = (props) => (
 	<header className="header">
 		<p><strong>Hello!</strong> This tool reads your MtG Arena draft deck from a log file and simulates opening hand draws for your deck testing purposes. </p>
-		<p>The default location of the files is: C:\Program Files (x86)\Wizards of the Coast\MTGA\MTGA_Data\Logs\Logs</p>
+		<p>The default location of the files is:</p> 
+		<p><strong>C:\Program Files (x86)\Wizards of the Coast\MTGA\MTGA_Data\Logs\Logs</strong></p>
 	</header>
 )
 
